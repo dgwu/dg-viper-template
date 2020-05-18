834aae130db0aa944d0925eef60e954605684b9c
DGWU <daniel.gunawan@bankmandiri.co.id>
2020-05-19 00:32:49 +0700

    fix linter issue

--- DgViper.xctemplate/___FILEBASENAME___Protocols.swift
+++ DgViper.xctemplate/___FILEBASENAME___Protocols.swift
@@ -5,26 +5,15 @@ import UIKit
 // MARK: View -
 protocol ___VARIABLE_viperModuleName___PresenterToView: class {
     var presenter: ___VARIABLE_viperModuleName___ViewToPresenter? { get set }
-    /**
-     * Stubs for communication PRESENTER -> VIEW
-     */
 }
 
 // MARK: Interactor -
 protocol ___VARIABLE_viperModuleName___PresenterToInteractor: class {
     var presenter: ___VARIABLE_viperModuleName___InteractorToPresenter? { get set }
-    
-    /**
-     * Stubs for communication PRESENTER -> INTERACTOR
-     */
 }
-
 
 // MARK: Router -
 protocol ___VARIABLE_viperModuleName___PresenterToRouter: class {
-    /**
-     * Stubs for communication PRESENTER -> ROUTER
-     */
 }
 
 // MARK: Presenter -
@@ -32,13 +21,7 @@ protocol ___VARIABLE_viperModuleName___ViewToPresenter: class {
     var view: ___VARIABLE_viperModuleName___PresenterToView? { get set }
     var interactor: ___VARIABLE_viperModuleName___PresenterToInteractor? { get set }
     var router: ___VARIABLE_viperModuleName___PresenterToRouter? { get set }
-    /**
-     * Stubs for communication VIEW -> PRESENTER
-     */
 }
 
 protocol ___VARIABLE_viperModuleName___InteractorToPresenter: class {
-    /**
-     * Stubs for communication INTERACTOR -> PRESENTER
-     */
 }
