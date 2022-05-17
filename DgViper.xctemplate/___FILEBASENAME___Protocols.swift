6f37e02bbe90030e5476087606854932287c1e5e
Ivan <ivan.aldinata@gmail.com>
2022-05-17 10:52:15 +0700

    Update ___FILEBASENAME___Protocols.swift
    change class to AnyObject

--- DgViper.xctemplate/___FILEBASENAME___Protocols.swift
+++ DgViper.xctemplate/___FILEBASENAME___Protocols.swift
@@ -3,25 +3,25 @@
 import UIKit
 
 // MARK: View -
-protocol ___VARIABLE_viperModuleName___PresenterToView: class {
+protocol ___VARIABLE_viperModuleName___PresenterToView: AnyObject {
     var presenter: ___VARIABLE_viperModuleName___ViewToPresenter? { get set }
 }
 
 // MARK: Interactor -
-protocol ___VARIABLE_viperModuleName___PresenterToInteractor: class {
+protocol ___VARIABLE_viperModuleName___PresenterToInteractor: AnyObject {
     var presenter: ___VARIABLE_viperModuleName___InteractorToPresenter? { get set }
 }
 
 // MARK: Router -
-protocol ___VARIABLE_viperModuleName___PresenterToRouter: class {
+protocol ___VARIABLE_viperModuleName___PresenterToRouter: AnyObject {
 }
 
 // MARK: Presenter -
-protocol ___VARIABLE_viperModuleName___ViewToPresenter: class {
+protocol ___VARIABLE_viperModuleName___ViewToPresenter: AnyObject {
     var view: ___VARIABLE_viperModuleName___PresenterToView? { get set }
     var interactor: ___VARIABLE_viperModuleName___PresenterToInteractor? { get set }
     var router: ___VARIABLE_viperModuleName___PresenterToRouter? { get set }
 }
 
-protocol ___VARIABLE_viperModuleName___InteractorToPresenter: class {
+protocol ___VARIABLE_viperModuleName___InteractorToPresenter: AnyObject {
 }
